8a8221749c5f28496c2b3c61bc566c1a0f438b9f
genagithub <143031972+genagithub@users.noreply.github.com>
2026-03-06 18:27:28 -0300

    commit

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ app.layout =  html.Div(id="body", className="e6_body", children=[
                         clearable=False),
     html.Div(id="graph_div_1", className="e6_graph_div_1", children=[
         html.Div(id="KPI_div_1", className="e6_KPI_div_1", children=[
-            html.P(["Max: ", max_conversion_b], className="e6_KPI_1", style={"margin-right":"25px","width":"300px"}),
-            html.P(["Min: ", min_conversion_b], className="e6_KPI_1", style={"margin-left":"25px","width":"300px"})
+            html.P(["Max:", " ", max_conversion_b], className="e6_KPI_1", style={"margin-right":"25px","width":"300px"}),
+            html.P(["Min:", " ", min_conversion_b], className="e6_KPI_1", style={"margin-left":"25px","width":"300px"})
         ]),
         dcc.Graph(id="conversions_analysis", figure={}, className="e6_graph_1", style={"width":"75%"})
     ]),
