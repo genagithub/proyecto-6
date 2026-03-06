8f7bf462ede912f2073d5417fbf91551517b3f56
genagithub <143031972+genagithub@users.noreply.github.com>
2026-03-06 19:08:24 -0300

    commit

--- app.py
+++ app.py
@@ -30,18 +30,20 @@ ROI = html.B(children=[], id="ROI")
 CVR = html.B(children=[], id="CVR")
 CPC = html.B(children=[], id="CPC")
 
+vars = {
+    "Tipo de campaña":"Campaign_Type",
+    "Compañía":"Company",
+    "Canal usado":"Channel_Used",
+    "Locación":"Location"
+}
+
 app = dash.Dash(__name__)
 server = app.server
 
 app.layout =  html.Div(id="body", className="e6_body", children=[
     html.H1("Análisis de variables de contexto", id="H1", className="e6_title"),
     dcc.Dropdown(id="dropdown_vars", style={"width":"160px"}, className="e6_dropdown_1",
-                        options=[
-                            {"label":"Tipo de campaña","value":"Campaign_Type"},
-                            {"label":"Compañía","value":"Company"},
-                            {"label":"Canal usado","value":"Channel_Used"},
-                            {"label":"Locación","value":"Location"}
-                        ],
+                        options=[{"label": v, "value": k} for k, v in vars.items()],
                         value="Campaign_Type",
                         multi=False,
                         clearable=False),
@@ -139,8 +141,9 @@ def update_graph(slct_var, slct_campaign, slct_company, slct_channel, slct_locat
         
     min_conversion = f"{var_min} ({int(value_min)})"
     max_conversion = f"{var_max} ({int(value_max)})"
-        
-    radar_chart = px.line_polar(df_grouped, r="Conversions", theta=slct_var, line_close=True, title="Éxito de Conversiones por Tipo de Campaña")
+
+    slct_label = vars[slct_var]
+    radar_chart = px.line_polar(df_grouped, r="Conversions", theta=slct_var, line_close=True, title=f"Éxito de Conversiones por {slct_var}")
     
     df_ts = df_segment.groupby("Date").agg({
         "Conversions": "sum",
