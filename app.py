f3397cf6d251160276e9220f34de6208074c317a
genagithub <143031972+genagithub@users.noreply.github.com>
2026-07-06 13:40:18 -0300

    commit

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def update_forecast(slct_var, slct_campaign, slct_company, slct_channel, slct_lo
         dummy_context_row["CPC"] = curr_cpc
         dummy_context_row["CPM"] = curr_cpm
         
-        dummy_context_row = dummy_context_row[final_feaures]
+        dummy_context_row = dummy_context_row[final_features]
         
         pred_conv = max(0, rf_conv.predict(dummy_context_row)[0]) 
         pred_roi = rf_roi.predict(dummy_context_row)[0]
